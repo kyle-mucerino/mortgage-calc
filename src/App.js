8061bd202feea672834689e39e4d89f6d0f83c32
kyle-mucerino <kyle.mucerino@gmail.com>
2023-08-17 12:09:38 -0700

    index.css changed to APp.css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./index.css"; 
+import "./App.css"; 
 
 function App() {
   const [loanAmount, setLoanAmount] = useState(""); 
